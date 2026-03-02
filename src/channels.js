79dc271dc7882601f907bea28c9ab7f7d2878225
Oabouhajar <smsm.sy@hotmail.com>
2026-03-02 18:07:39 +0000

    feat: add default news channels to the channel list

--- src/channels.js
+++ src/channels.js
@@ -33,6 +33,66 @@ export const DEFAULT_CHANNELS = [
     channelId: 'UC16niRr50-MSBwiO3YDb3RA',
     color: '#bb1919',
   },
+  {
+    name: 'CNN',
+    handle: '@CNN',
+    channelId: 'UCupvZG-5ko_eiXAupbDfxWw',
+    color: '#cc0000',
+  },
+  {
+    name: 'Fox News',
+    handle: '@FoxNews',
+    channelId: 'UCXIJgqnII2ZOINSWNOGFThA',
+    color: '#003366',
+  },
+  {
+    name: 'MSNBC',
+    handle: '@MSNBC',
+    channelId: 'UCaXkIU1QidjPwiAYu6GcHjg',
+    color: '#0b5394',
+  },
+  {
+    name: 'Sky News',
+    handle: '@SkyNews',
+    channelId: 'UCoMdktPbSTixAyNGwb-UYkQ',
+    color: '#e31e26',
+  },
+  {
+    name: 'ABC News',
+    handle: '@ABCNews',
+    channelId: 'UCBi2mrWuNuyYy4gbM6fU18Q',
+    color: '#0066cc',
+  },
+  {
+    name: 'CBS News',
+    handle: '@CBSNews',
+    channelId: 'UC8p1vwvWtl6T73JiExfWs1g',
+    color: '#1a1a2e',
+  },
+  {
+    name: 'NBC News',
+    handle: '@NBCNews',
+    channelId: 'UCeY0bbntWzzVIaj2z3QigXg',
+    color: '#ff6600',
+  },
+  {
+    name: 'France 24',
+    handle: '@FRANCE24English',
+    channelId: 'UCQfwfsi5VrQ8yKZ-UWmAEFg',
+    color: '#0055a4',
+  },
+  {
+    name: 'DW News',
+    handle: '@DWNews',
+    channelId: 'UCknLrEdhRCp1aegoMqRaCZg',
+    color: '#00a0de',
+  },
+  {
+    name: 'Reuters',
+    handle: '@Reuters',
+    channelId: 'UChqUTb7kYRX8-EiaN3XFrSQ',
+    color: '#ff8000',
+  },
 ];
 
 /** Known handle → channelId mapping for auto-migration */
